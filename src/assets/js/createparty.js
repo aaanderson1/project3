bc85561f5d9d3fe0db7592808fa06fc89031127a
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-08-14 23:15:10 -0600

    added all jumbotron images

--- src/assets/js/createparty.js
+++ src/assets/js/createparty.js
@@ -7,6 +7,82 @@ $(document).ready(function () {
     });
     $(".generalbabypic").click(function(){
         $(".jumbotronpic").attr("src", "assets/images/baby-shower-neutral.jpg")
-        console.log("general baby pic clicked")
       });
+    $(".boybaby").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/babyblue.jpg")
+      });
+    $(".girlbaby").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/babypink.jpg")
+    });
+    $(".bluecake").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/bluecake.jpg")
+      });
+    $(".blueglitter").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/blueglitter.jpg")
+      });
+    $(".champagne").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/champagne.jpg")
+      });
+      $(".coloredlights").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/coloredlights.jpg")
+      });
+    $(".cremewedding").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/creamcake.jpg")
+      });
+    $(".elegantwedding").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/sophisticated-wedding.jpg")
+      });
+    $(".pinkroseswood").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/pinkwood.jpg")
+      });
+    $(".firework").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/blackgoldparty.jpg")
+      });
+    $(".goldglitter").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/goldglitter.jpg")
+      });
+    $(".lightbulb").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/lightbulb.jpg")
+      });
+    $(".multicolor").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/abstract.jpg")
+      });
+    $(".pinkcake").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/pinkcake.jpg")
+      });
+    $(".pinkrose").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/lightpinkroses.jpg")
+      });
+    $(".pinkroses").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/pinkroses.jpg")
+      });
+    $(".rainbowwood").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/rainbowwood.jpg")
+      });
+    $(".redrose").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/redrose.jpg")
+      });
+    $(".silver").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/silver.jpg")
+      });
+    $(".silverwedding").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/whiteroses.jpg")
+      });
+    $(".stringlights").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/stringlights.jpg")
+      });
+     $(".weddingrings").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/weddingrings.jpg")
+      });
+    $(".beachwedding").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/beachwedding.jpg")
+      });
+    $(".purplewedding").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/purplewedding.jpg")
+      });
+    $(".rainbowroses").click(function(){
+        $(".jumbotronpic").attr("src", "assets/images/rainbowroses.jpg")
+      });
+    
+
 })
